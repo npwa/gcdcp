32fdeec1140dea1993705c3344a7de207af493e6
Nikolaus Almassy <nalmassy@yahoo.com>
2018-06-10 18:42:53 -0700

    corrected script version

--- CodeBook.md
+++ CodeBook.md
@@ -1,13 +1,23 @@
+# Code Book
 
-The variable "train" contains the merged training dataset from the
-following files:
+This project contains the resulting merged data after tidying it up from the original files
 
- train/X_train.txt
- train/y_train.txt
- train/subject_train.txt
+* train/X_train.txt
+* train/y_train.txt
+* train/subject_train.txt
 
 It contains all the 7352 observations of 563 variables (Subject and Activity was added in column 1 and 2)
  Subject : int  1 1 1 1 1 1 1 1 1 1 ...
  Activity: int  5 5 5 5 5 5 5 5 5 5 ...
  V1      : num  0.289 0.278 0.28 0.279 0.277 ...
  [...]
+
+
+## Activity Labels
+
+* "WALKING" (value=1): Subject was walking during the test
+* "WALKING_UPSTAIRS" (value=2): Subject was walking up a staircase during the test
+* "WALKING_DOWNSTAIRS" (value=3): Subject was walking down a staircase during the test
+* "SITTING" (value=4): Subject was sitting during the test
+* "STANDING" (value=5): Subject was standing during the test
+* "LAYING" (value=6): Subject was laying down during the test
--- run_analysis.R
+++ run_analysis.R
@@ -1,30 +1,62 @@
 ##
 ## The script run_analysis.R does the following:
 ##
-##  1  Merges the training and the test sets to create one data set.
-##  2  Extracts only the measurements on the mean and standard deviation for each measurement.
-##  3  Uses descriptive activity names to name the activities in the data set
-##  4  Appropriately labels the data set with descriptive variable names.
+##  1 Merges the training and the test sets to create one data set.
+##  2 Extracts only the measurements on the mean and standard deviation for each measurement.
+##  3 Uses descriptive activity names to name the activities in the data set
+##  4 Appropriately labels the data set with descriptive variable names.
 ##  5 From the data set in step 4, creates a second, independent tidy
 ##    data set with the average of each variable for each activity and
 ##    each subject.
 
-FilePath="/home/nick/projects/Coursera/Getting-and-Cleaning-Data/UCI_HAR_Dataset/"
-train_activity <- read.table(paste0(FilePath,"train/y_train.txt")) ## 7352 lines
-colnames(train_activity) <- "Activity"
+source_file <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+filename <- basename(source_file)
+download.file(source_file, filename, method="curl")
+unzip(filename)
+setwd("UCI HAR Dataset")
 
-labels <- read.table(paste0(FilePath,"activity_labels.txt"))
-colnames(labels) <- c("Activity","Activity_name")
+#
+# Read in features and activity labels
+labels <- read.table("activity_labels.txt")
+labels[,2] <- as.character(labels[,2])
+
+features <- read.table("features.txt")
+features[,2] <- as.character(features[,2])
 
 
-xtrain <- read.table(paste0(FilePath,"train/X_train.txt"))
-subject <- read.table(paste0(FilePath,"train/subject_train.txt"))
-colnames(subject) <- "Subject"
-train <- cbind(subject,train_activity,xtrain)
+#
+# Get the indicies of the data for mean and stdev simplify the names
+# of the required features (remove dashes and parenthesis, capitalize
+# the name)
+required_features <- grep(".*mean.*|.*std.*", features[,2])
+required_features.names <- features[required_features,2]
+required_features.names = gsub('-mean', 'Mean', required_features.names)
+required_features.names = gsub('-std', 'Std', required_features.names)
+required_features.names <- gsub('[-()]', '', required_features.names)
 
-## > str(train)
-## 'data.frame':	7352 obs. of  563 variables:
-##  $ Subject : int  1 1 1 1 1 1 1 1 1 1 ...
-##  $ Activity: int  5 5 5 5 5 5 5 5 5 5 ...
-##  $ V1      : num  0.289 0.278 0.28 0.279 0.277 ...
-##  [...]
+
+# Read the training dataset, add subject and activities column
+train <- read.table("train/X_train.txt")[required_features]
+train_activities <- read.table("train/y_train.txt")
+train_subjects <- read.table("train/subject_train.txt")
+train <- cbind(train_subjects, train_activities, train)
+
+# Read the test dataset, add subject and activities column
+test <- read.table("test/X_test.txt")[required_features]
+test_activities <- read.table("test/y_test.txt")
+test_subjects <- read.table("test/subject_test.txt")
+test <- cbind(test_subjects, test_activities, test)
+
+# Merge the two datasets and add labels
+all_data <- rbind(train, test)
+colnames(all_data) <- c("Subject", "Activity", required_features.names)
+
+#
+# Convert Activities and Subjects into factors
+all_data$Activity <- factor(all_data$Activity, levels = labels[,1], labels = labels[,2])
+all_data$Subject <- as.factor(all_data$Subject)
+
+all_data.melted <- melt(all_data, id = c("Subject", "Activity"))
+all_data.mean <- dcast(all_data.melted, Subject + Activity ~ variable, mean)
+
+write.table(all_data.mean, "tidy_data.txt", row.names = FALSE, quote = FALSE)
